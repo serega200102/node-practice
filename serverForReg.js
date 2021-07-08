70ecb55a93eeac74f17ad3a9a983579bd97acfac
Sergey <serega200102@gmail.com>
2021-07-09 02:40:35 +0300

    Убрал лишние считывания из файла

--- serverForReg.js
+++ serverForReg.js
@@ -7,6 +7,26 @@ const {parse} = require ('querystring');
 let obj;
 let objmas = [];
 let objmas2 = [];
+let readtext = new Promise(function(resolve, reject){  
+   fs.readFile("./static/Test.txt", "utf8", function(error,data){
+        if(error){
+           console.log('бедаа')
+           reject();
+        }else{
+           resolve(data);
+           console.log('Скопировали данные перед запуском сервера')
+        }  
+     })
+  });
+  
+  readtext.then((data) => {
+     objmas.push(data);
+     objmas = JSON.parse(objmas)
+     console.log('Информация загружена и обработана в json')
+      }).catch(()=>{
+         console.log("Troble")
+      })
+  
 const server = http.createServer((req, res) => {
    
 
@@ -16,87 +36,36 @@ const server = http.createServer((req, res) => {
 
       if(req.url == '/log.html'){
          collectRequestData(req, result => {
-            
-            
-         let readtext = new Promise(function(resolve, reject){  
-            fs.readFile("./static/Test.txt", "utf8", function(error,data){
-                 if(error){
-                    console.log('бедаа')
-                    reject();
-                 }else{
-                    console.log(typeof(JSON.parse(data)))
-                    let a = JSON.parse(data)
-                    resolve(a);
-                    console.log('все ок')
-                   
-                 }  
-              })
-           });
-            readtext.then((data) => {
-               let index = -1;
-               console.log('тут инфа из сервера')
-               let info = data ;
-               console.log(info)
-               console.log(result)
-               console.log(result.password)
-               for(let i = 0; i<info.length; i++ ){
-                  if(result.email == info[i].email){
-                     index = i;
-                  } 
-               }
-               result.password = crypto.createHash('md5').update(result.password).digest('hex');
+         let index = -1;
+         console.log('тут инфа из сервера')
+         console.log(objmas)
+         console.log(result)
+         console.log(result.password)
+         for(let i = 0; i<objmas.length; i++ ){
+            if(result.email == objmas[i].email){
+               index = i;
+               } 
+            }
+            result.password = crypto.createHash('md5').update(result.password).digest('hex');
               // console.log(result.password)
-             if(index == -1){
-                  console.log('нет такого email')
-               }
-               else if(result.password == info[index].password) {
-                  console.log('ВЫ ЗАШЛИ!!')
-                  sendRes("site.html","text/html", res );
-               }
-               else{
-                  console.log('пароль не верный')
-               }
-            
-             }).catch(()=>{
-                console.log("Troble")
-             })
-            });
+            if(index == -1){
+               console.log('нет такого email')
+            }
+            else if(result.password == objmas[index].password) {
+               console.log('ВЫ ЗАШЛИ!!')
+               sendRes("site.html","text/html", res );
+            }
+            else{
+               console.log('пароль не верный')
+            }  
+         });
       }
       else{
          collectRequestData(req, result => {
                //console.log(result.password)
-               result.password = crypto.createHash('md5').update(result.password).digest('hex');
-               let readtext2 = new Promise(function(resolve, reject){  
-                  fs.readFile("./static/Test.txt", "utf8", function(error,data){
-                       if(error){
-                          console.log('бедаа')
-                          reject();
-                       }else{
-                        console.log('файл считался');
-                        resolve(data)
-                       }  
-                    })
-                 });
-
-               readtext2.then((data) => {
+                  result.password = crypto.createHash('md5').update(result.password).digest('hex');
                   let a = []
-                  objmas = [];
-                  console.log(data)
-                  console.log(objmas)
-                  console.log('start')
-                  //console.log(JSON.parse(data))
-                  a = JSON.parse(data);
-                  console.log(a.length)
-                  console.log(objmas)
-                 // console.log(result)
-                  for(let i = 0; i <= a.length-1; i++){
-                     console.log('bu')
-                     console.log(i)
-                     objmas.push(a[i])
-                     
-                  }
-                  console.log('info')
-                  console.log(objmas)
+                  console.log('Записываем данные нового клиента')
                   //console.log(a)
                   //objmas.push(JSON.parse(data))
                   objmas.push((result))
@@ -111,20 +80,6 @@ const server = http.createServer((req, res) => {
                      } 
                   })
 
-               }).catch(()=>{
-                  console.log("Trobble")
-               })
-               /*fs.appendFile("./static/Test.txt",JSON.stringify(result), function(error){
-                  if(error){
-                     console.log('бедаа')
-                     console.log(error)
-                     
-                  }else{
-
-                     console.log(result)
-                     console.log('записано')
-                  } 
-               })*/
                sendRes("reg.html","text/html", res );
         });
       }
@@ -162,7 +117,7 @@ function collectRequestData(request, callback) {
 }
 
 
-function getContentType(url){
+function getContentType(url){ //Функция для получения нужного типа данных для разных файлов
    /* switch(path.extname(url)){
        case '.html':
           return "text/html";
@@ -186,7 +141,7 @@ function getContentType(url){
  }
 
 
- function sendRes( url, contentType, res ){
+ function sendRes( url, contentType, res ){     //ФУНКЦИЯ ДЛЯ ПЕРЕХОДА НА НОВЫЙ get ЗАПРОС
    let file = path.join(__dirname , 'static', url);
    fs.readFile(file,'utf8',(err, data)=>{
       if(err){
@@ -204,7 +159,7 @@ function getContentType(url){
       }
    })
 }
-function getform(req,res){
+function getform(req,res){    //ФУНКЦИЯ ДЛЯ ЛОГИКИ ПЕРЕХОДОВ МЕЖДУ СТРАНИЦАМИ
    if(req.url ==='/'){
       sendRes("reg.html","text/html", res );
    }
@@ -219,11 +174,4 @@ function getform(req,res){
       console.log(getContentType(req.url))
       sendRes(req.url,getContentType(req.url) , res);
    }
-}
-
-function readTxt(){
-   let info = fs.readFile("./static/Test.txt", "utf8", function(error,data){ // если возникла ошибка
-      if(error) throw error;
-      return data
-    });  
 }
