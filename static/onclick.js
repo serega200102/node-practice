f0a24342d5f16b763af01108946dac6790fe3131
Sergey <serega200102@gmail.com>
2021-07-30 08:31:11 +0300

    Checkbox

--- static/onclick.js
+++ static/onclick.js
@@ -1,6 +1,9 @@
 btm.onclick = function() {
+   //console.log(document.querySelector('#scales1'))
    if(document.getElementById('password').value != document.getElementById('password2').value){
       alert('Пароли не совпадают')
       return false;
    }
  };
+
+
